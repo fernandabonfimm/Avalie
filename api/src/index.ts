989d361ed3b3ae41bba73fb9675123c4613cd1d4
Fernanda Bonfim <82246157+fernandabonfimm@users.noreply.github.com>
2023-08-17 13:42:27 -0300

    iniciando a api

---
+++ api/src/index.ts
@@ -0,0 +1,11 @@
+const express = require('express');
+require("dotenv").config();
+
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em: ${port}`);
+});
